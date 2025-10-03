08d53e67f50ebc7d40917de3a3e58b50c6a8a5ad
pradeepkumarv <bengaluru.pradeep@gmail.com>
2025-10-03 20:28:40 +0530

    Delete export-functions-fix.js

--- export-functions-fix.js
+++
@@ -1,5 +0,0 @@
-function exportReport(format, category) {
-    if (format === 'pdf') exportToPDF(category);
-    else if (format === 'csv') exportToCSV(category);  
-    else if (format === 'json') exportToJSON(category);
-}
